4c797d59dbf270ba00cac7342291a5ece7ef1381
RoaaMustafa <labushanab14@gmail.com>
2021-06-16 17:10:12 +0300

    editing

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,8 +3,10 @@ import WeatherDay from "./WeatherDay";
  class Weather extends React.Component {
     render() {
         return(
+            <div className="weather-sec">
         <WeatherDay 
         descWeather={this.props.seeWeathetState}/>
+        </div>
         );
     }
 }
